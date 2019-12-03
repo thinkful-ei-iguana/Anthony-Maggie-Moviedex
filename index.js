29a961a9356e263c44529b6e5b420764e57984bb
AnthonyTB <Anthonylol@protonmail.com>
2019-12-03 13:23:54 -0800

    token handling and some possible user input error handling

--- index.js
+++ index.js
@@ -3,70 +3,53 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-
 const movies = require('./movies');
-
 const app = express();
+const API_Key = process.env.API_TOKEN;
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(helmet());
 
+// Begin Auth
+app.use(function validatonCheckpoint(req, res, next) {
+  const User_API_Key = req.get('Authorization');
+  console.log(User_API_Key);
+  if (!User_API_Key || User_API_Key.split(' ')[1] !== API_Key) {
+    return res.status(401).json({ error: 'Not a valid API key' });
+  }
+  next();
+});
+
+// End Auth
+
 app.get('/movies', (req, res) => {
   const { genre, country, avg_vote } = req.query;
+  let response;
 
   if (req.query.genre) {
     response = movies.filter(movie =>
-      movie
-        .genre
+      movie.genre
         .toLowerCase()
-        .includes(req.query.genre.toLowerCase())
-    )
+        .includes(req.query.genre.toLowerCase().replace('_', ' '))
+    );
   }
 
   if (req.query.country) {
     response = movies.filter(movie =>
-      movie
-        .country
+      movie.country
         .toLowerCase()
-        .includes(req.query.country.toLowerCase())
-    )
+        .includes(req.query.country.toLowerCase().replace('_', ' '))
+    );
   }
 
   if (req.query.avg_vote) {
-    response = movies.filter(movie =>
-      movie.avg_vote >= (req.query.avg_vote)
-    )
+    response = movies.filter(movie => movie.avg_vote >= req.query.avg_vote);
   }
 
   res.json(response);
-
-  // let searchMethod;
-  // let searchTerm;
-
-  // if (genre) {
-  //   searchMethod = 'genre';
-  // }
-  // if (country) {
-  //   searchMethod = 'country';
-  // }
-  // if (avg_vote) {
-  //   searchMethod = 'avg_vote';
-  // }
-
-  // const filter = movies.filter(movie => {
-  //   let userInput = Object.values(req.query)
-  //   console.log('userInput is', userInput[0]);
-  //   console.log('movie.country is', movie.country);
-  //   movie.searchMethod === userInput[0];
-  // })
-  // console.log('filter is', filter);
-  // let response = filter;
-
-  // res.json(response);
-})
+});
 
 app.listen(8050, () => {
   console.log('listening at PORT 8050');
 });
-
